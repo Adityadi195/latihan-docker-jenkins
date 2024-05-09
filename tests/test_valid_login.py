8319410b938d3b32562aeb22e7ad3ab23800b15e
Adityadi195 <adityadi230@gmail.com>
2024-05-09 23:42:42 +0700

    selenium grid

--- tests/test_valid_login.py
+++ tests/test_valid_login.py
@@ -2,9 +2,23 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(50)
-driver.maximize_window()
+# URL untuk Selenium Grid Hub
+hub_url = "http://172.17.0.5:4444"
+
+# Konfigurasi capability untuk browser yang akan dijalankan
+capabilities = {
+    "browserName": "chrome"
+}
+
+# Inisialisasi WebDriver dengan menggunakan URL Selenium Grid Hub dan capability
+driver = webdriver.Remote(
+    command_executor=hub_url,
+    options=webdriver.ChromeOptions(), # options argument is mandatory
+)
+
+# Jeda untuk memberikan waktu kepada Selenium Grid Node untuk menjalankan browser
+time.sleep(5)
+
 driver.get("https://www.saucedemo.com/")
 
 time.sleep(2)
